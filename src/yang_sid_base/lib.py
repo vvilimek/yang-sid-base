0269dab2840125d7a88041c824f1a719b85fd2d1
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-05-14 16:25:05 +0200

    Fix bugs

--- src/yang_sid_base/lib.py
+++ src/yang_sid_base/lib.py
@@ -2,24 +2,23 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from typing import NewType, Optional, overload, NoReturn, Union
+from typing import NewType, Optional, overload, NoReturn, Union, Self
 
 __all__ = ("SID", "AbsoluteSID", "RelativeSID")
 
 AbsoluteSID = NewType("AbsoluteSID", int)
 
-def SID(int):
+class SID(int):
     @staticmethod
-    def check(sid: int) -> Optional[AbsouteSID]:
-        if sid <= 0 or sid >= 2**63:
+    def check(sid: int) -> Optional[AbsoluteSID]:
+        if sid < 0 or sid >= 2**63:
             return None
         return AbsoluteSID(sid)
 
-    def __init__(self, sid: int) -> None:
-        value = SID.check(sid)
-        if value is None:
+    def __new__(cls, sid: int) -> Self:
+        if SID.check(sid) is None:
             raise ValueError(f"Invalid value for a SID number {sid}")
-        super().__init__(sid)
+        return super().__new__(cls, sid)
 
     @overload
     def __add__(self, other: "RelativeSID") -> "SID": ...
@@ -27,7 +26,7 @@ def SID(int):
     def __add__(self, other: object) -> NoReturn: ...
 
     def __add__(self, other: object) -> Union["SID", NoReturn]:
-        if not isintance(other, RelativeSID):
+        if not isinstance(other, RelativeSID):
             raise TypeError
         return SID(int(self) + int(other))
 
@@ -37,11 +36,11 @@ def SID(int):
     def __sub__(self, other: object) -> NoReturn: ...
 
     def __sub__(self, other: object) -> Union["RelativeSID", NoReturn]:
-        if not isinstance(other, AbsoluteSID):
+        if not isinstance(other, int):
             raise TypeError
         return RelativeSID(int(self) - int(other))
 
 class RelativeSID(int):
-    def __init__(self, relative: int) -> None:
-        super().__init__(relative)
+    def __new__(cls, relative: int) -> Self:
+        return super().__new__(cls, relative)
 
