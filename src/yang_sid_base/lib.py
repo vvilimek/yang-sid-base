5163eb76b61b8aeb84999445fab4c58bd5416239
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-05-14 10:09:47 +0200

    SID checking function

--- src/yang_sid_base/lib.py
+++ src/yang_sid_base/lib.py
@@ -2,14 +2,22 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from typing import NewType
+from typing import NewType, Optional
 
-__all__ = ("SID", "AbsoluteSID", "RelativeSID")
+__all__ = ("SID", "AbsoluteSID", "RelativeSID", "check_sid")
 
 SID = NewType("SID", int)
 
+def check_sid(sid: int) -> Optional[SID]:
+    if sid <= 0 or sid >= 2**63:
+        return None
+    return SID(sid)
+
 class AbsoluteSID(int):
-    pass
+    def __init__(self, sid: SID) -> None:
+        super().__init__(sid)
 
 class RelativeSID(int):
-    pass
+    def __init__(self, relative: int) -> None:
+        super().__init__(relative)
+
