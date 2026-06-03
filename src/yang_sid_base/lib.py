7c1463e5c65adb034030582d2fedc13c7a7cbc73
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-06-03 12:17:47 +0200

    Finish SID, RelativeSID arithmetic

--- src/yang_sid_base/lib.py
+++ src/yang_sid_base/lib.py
@@ -20,6 +20,12 @@ class SID(int):
             raise ValueError(f"Invalid value for a SID number {sid}")
         return super().__new__(cls, sid)
 
+    def __str__(self) -> str:
+        return f"SID({int(self)})"
+
+    def __repr__(self) -> str:
+        return f"yang_sid_base.lib.SID({super().__repr__()})"
+
     @overload
     def __add__(self, other: "RelativeSID") -> "SID": ...
     @overload
@@ -31,28 +37,129 @@ class SID(int):
         return SID(int(self) + int(other))
 
     @overload
+    def __radd__(self, other: "RelativeSID") -> "SID": ...
+    @overload
+    def __radd__(self, other: object) -> NoReturn: ...
+
+    def __radd__(self, other: object) -> Union["SID", NoReturn]:
+        if not isinstance(other, RelativeSID):
+            raise TypeError
+        return SID(int(other) + int(self))
+
+    @overload
     def __sub__(self, other: "SID") -> "RelativeSID": ...
+    @overload
+    def __sub__(self, other: "RelativeSID") -> "SID": ...
     @overload
     def __sub__(self, other: object) -> NoReturn: ...
 
-    def __sub__(self, other: object) -> Union["RelativeSID", NoReturn]:
-        if not isinstance(other, int):
+    def __sub__(self, other: object) -> Union["RelativeSID", "SID", NoReturn]:
+        if isinstance(other, SID):
+            return RelativeSID(int(self) - int(other))
+        elif isinstance(other, RelativeSID):
+            return SID(int(self) - int(other))
+        else:
             raise TypeError
-        return RelativeSID(int(self) - int(other))
+
+    @overload
+    def __rsub__(self, other: "SID") -> "RelativeSID": ...
+    @overload
+    def __rsub__(self, other: "RelativeSID") -> "SID": ...
+    @overload
+    def __rsub__(self, other: object) -> NoReturn: ...
+
+    def __rsub__(self, other: object) -> Union["RelativeSID", "SID", NoReturn]:
+        if isinstance(other, SID):
+            return RelativeSID(int(other) - int(self))
+        elif isinstance(other, RelativeSID):
+            return SID(int(other) - int(self))
+        else:
+            raise TypeError
+        
+    @overload
+    def __eq__(self, other: "SID") -> bool: ...
+    @overload
+    def __eq__(self, other: object) -> NoReturn: ...
+
+    def __eq__(self, other: object) -> Union[bool, NoReturn]:
+        if isinstance(other, SID):
+            return int(self) == int(other)
+        else:
+            raise TypeError
+
+    def __neg__(self) -> Self:
+        raise TypeError()
 
     def to_int(self) -> int:
-        return int(self)
+        return super().__int__()
 
     def __cbor__(self) -> int:
-        return self.to_int()
+        return int(self)
 
 class RelativeSID(int):
     def __new__(cls, relative: int) -> Self:
         return super().__new__(cls, relative)
 
+    def __str__(self) -> str:
+        return f"RelativeSID({int(self)})"
+
+    def __repr__(self) -> str:
+        return f"yang_sid_base.lib.RelativeSID({super().__repr__()})"
+
+    def __neg__(self) -> Self:
+        return RelativeSID(-int(self))
+
+    @overload
+    def __add__(self, other: SID) -> SID: ...
+    @overload
+    def __add__(self, other: object) -> NoReturn: ...
+
+    def __add__(self, other: object) -> Union[SID, NoReturn]:
+        if isinstance(other, SID):
+            return SID(int(self) + int(other))
+        else:
+            raise TypeError
+
+    @overload
+    def __radd__(self, other: SID) -> SID: ...
+    @overload
+    def __radd__(self, other: object) -> NoReturn: ...
+
+    def __radd__(self, other: object) -> Union[SID, NoReturn]:
+        if isinstance(other, SID):
+            return SID(int(other) + int(self))
+        else:
+            raise TypeError
+
+    def __sub__(self, other: object) -> NoReturn:
+        # If the other operand in an expresion a - b is SID then (a) + (-b), (-b) SID is not valid
+        raise TypeError
+
+    @overload
+    def __rsub__(self, other: SID) -> SID: ...
+    @overload
+    def __rsub__(self, other: object) -> NoReturn: ...
+
+    def __rsub__(self, other: object) -> Union[SID, NoReturn]:
+        if isinstance(self, SID):
+            return SID(int(other) - int(self))
+        else:
+            raise TypeError
+
+    @overload
+    def __eq__(self, other: "RelativeSID") -> bool: ...
+    @overload
+    def __eq__(self, other: object) -> NoReturn: ...
+
+    def __eq__(self, other: object) -> Union[bool, NoReturn]:
+        if isinstance(other, RelativeSID):
+            return int(self) == int(other)
+        else:
+            raise TypeError
+
     def to_int(self) -> int:
-        return int(self)
+        return super().__int__()
 
     def __cbor__(self) -> int:
-        return self.to_int()
+        return int(self)
 
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -29,8 +29,73 @@ def test_sid_check():
 def test_arit():
     root = sid.SID(5)
     child = sid.SID(7)
+    rel = sid.RelativeSID(5)
 
     assert child - root == sid.RelativeSID(2)
+    assert root - child == sid.RelativeSID(-2)
     assert root + sid.RelativeSID(2) == child
     assert child - sid.RelativeSID(2) == root
+    assert sid.RelativeSID(2) + root == child
+    assert -sid.RelativeSID(2) + child == root
+
+    assert root + (child - root) == child
+    assert child + (root - child) == root
+    assert (child - root) + root == child
+    assert (root - child) + child == root
+
+    assert type(sid.SID(2) + sid.RelativeSID(3)) == sid.SID
+    assert type(sid.RelativeSID(3) + sid.SID(2)) == sid.SID
+    assert type(sid.SID(2) - sid.SID(1)) == sid.RelativeSID
+    assert type(sid.SID(3) - sid.RelativeSID(1)) == sid.SID
+    assert type(-sid.RelativeSID(5)) == sid.RelativeSID
+
+    assert sid.RelativeSID(-3) == -sid.RelativeSID(3)
+
+    with pytest.raises(TypeError):
+        r1 = root + 1
+
+    with pytest.raises(TypeError):
+        r2 = root - 1
+
+    with pytest.raises(TypeError):
+        r3 = rel + 1
+
+    with pytest.raises(TypeError):
+        r4 = rel - 1 
+
+    with pytest.raises(TypeError):
+        r5 = root == 1
+
+    with pytest.raises(TypeError):
+        r6 = rel == 1
+
+    with pytest.raises(TypeError):
+        r7 = root == rel
+
+    with pytest.raises(TypeError):
+        r8 = 1 + root
+
+    with pytest.raises(TypeError):
+        r9 = 1 - root
+
+    with pytest.raises(TypeError):
+        r10 = 1 + rel
+
+    with pytest.raises(TypeError):
+        r11 = 1 - rel
+
+    with pytest.raises(TypeError):
+        r12 = 1 == root
+
+    with pytest.raises(TypeError):
+        r13 = 1 == rel
+
+    with pytest.raises(TypeError):
+        r14 = rel == root
+
+    with pytest.raises(TypeError):
+        r15 = -sid.SID(2)
+
+    with pytest.raises(TypeError):
+        r16 = rel - sid.SID(3)
 
